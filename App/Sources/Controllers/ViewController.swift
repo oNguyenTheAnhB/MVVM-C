08ab161a86afc23076b8c7eb1fa243c1b78ac06c
nguyen.the.anhb <nguyen.the.anh-b@sun-asterisk.com>
2021-03-05 09:00:10 +0700

    AbstractViewController

--- App/Sources/Controllers/ViewController.swift
+++ App/Sources/Controllers/ViewController.swift
@@ -1,5 +1,28 @@
 import UIKit
 
+// All ViewControllers should inherit this class.
 class ViewController: UIViewController {
-    
+    weak var weakCoordinator: Coordinator?
+
+    deinit {
+        weakCoordinator?.stop()
+    }
+}
+
+extension ViewController: CoordinatedController {
+    var coordinator: Coordinator? {
+        get {
+            return weakCoordinator
+        } set(coordinator) {
+            weakCoordinator = coordinator
+        }
+    }
+
+    func pushableCoordinator() -> (Coordinator & Pushable)? {
+        if let pushable = coordinator as? (Coordinator & Pushable) {
+            return pushable
+        }
+        assertionFailure("This coordinator has not adopted `Pushable` in \(String(describing: self)). Don't use pushable in this controller.")
+        return nil
+    }
 }
